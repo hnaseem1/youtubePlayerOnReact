248820058e6c3913230015c312bbe788c77019c1
Hasan Naseem <hn92@live.com>
2018-06-23 13:56:24 -0400

    refactored some more

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import VideoListItem from './video_list_item'
 
-
 const VideoList = (props) => {
+
   const videoItems = props.videos.map((video) => {
     return <VideoListItem key={video.etag} video={video} />
   })
 
   return (
+
     <ul>
       {videoItems}
     </ul>
+
   );
 };
 
