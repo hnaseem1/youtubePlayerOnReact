64a0a1396cd9012131ad330f017a4ac0dfc5ef59
Hasan Naseem <hn92@live.com>
2018-06-23 13:55:16 -0400

    refactored some code

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,34 +1,28 @@
 import React, { Component } from 'react';
 
-// { Componenet } is const Component = React.Componenet
-
-//funtional componenets
-// const SearchBar = () => {
-//     return <input />
-// };
-
-// creates a new class called SearchBar with access to all the methods from React
-
 // class based compoenent
 class SearchBar extends Component {
 
     constructor(props) {
-        super(props);
+      super(props);
 
-        this.state = { term: ''}
+      // defining state
+      this.state = { term: ''}
+
     }
-    // all componenets must have a render method
+
+    // all class-based componenets must have a render method
     render() {
-        return(
-        <div>
-         <input
-         value={this.state.term}
-         onChange={event => this.setState({ term: event.target.value })} />
-        </div>
-        );
-    }
+      return (
 
-}
+        <div>
+          <input value={this.state.term}
+            onChange={event => this.setState({ term: event.target.value })} />
+        </div>
+
+      );
+    }
+};
 
 // makes it available for other components
 export default SearchBar;
